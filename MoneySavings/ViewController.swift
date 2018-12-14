5532b8b2ed8e29d5d4b5e8a2bfcf7cce77d3fe3b
Daniele <daniele.piergigli@gmail.com>
2018-12-14 22:19:30 +0100

    Initial Commit

---
+++ MoneySavings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneySavings
+//
+//  Created by Daniele Piergigli on 14/12/2018.
+//  Copyright © 2018 Daniele Piergigli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
